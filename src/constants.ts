32f79e0e6e2775900300575cb6a98906bddb016f
Shubham H <shubhamhirakki@gmail.com>
2023-10-06 20:29:35 +0530

    Initial commit

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+const modelConstants = {
+  userModelName: 'User',
+  postModelName: 'Post',
+  commentModelName: 'Comment',
+  likeModelName: 'Like',
+  messageModelName: 'Message',
+};
+
+export default modelConstants;
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import routes from './routes';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT as string;
+const DB_URI = process.env.MONGODB_URI as string;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: `${process.env.BASE_URL}`,
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+
+mongoose
+  .connect(DB_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+app.use('/api', routes.authRouter);
+app.use('/api', routes.postRouter);
+app.use('/api', routes.userRouter);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
